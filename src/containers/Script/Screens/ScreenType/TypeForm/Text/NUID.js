403e158b5c3914bb7023ef6236de89408da57f45
Farai <farai@bwsonline.co.za>
2020-06-23 16:31:30 +0200

    Neotree ID

--- src/containers/Script/Screens/ScreenType/TypeForm/Text/NUID.js
+++ src/containers/Script/Screens/ScreenType/TypeForm/Text/NUID.js
@@ -1,60 +1,110 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { View } from 'react-native';
-import { Input, Form, Item } from 'native-base';
+import { Input, Form, Item } from '@/components/Form';
 import Text from '@/components/Text';
 
 const NUID = ({ field, onChange, value, conditionMet, }) => {
-  const [error] = React.useState(null);
+  const [_firstHalf, _lastHalf] = (value || '').split('-');
+
+  const firstHalfRef = React.useRef(null);
+  const lastHalfRef = React.useRef(null);
+
+  const [firstHalf, setFirstHalf] = React.useState(_firstHalf || '');
+  const [lastHalf, setLastHalf] = React.useState(_lastHalf || '');
+
+  const allowedFirstHalf = /^[a-fA-F0-9]*$/gi;
+  const allowedLastHalf = /^[0-9]*$/gi;
+  const _value = `${firstHalf}-${lastHalf}`;
+
+  React.useEffect(() => {
+    if (firstHalf.length === 4) lastHalfRef.current._root.focus();
+  }, [firstHalf]);
+
+  React.useEffect(() => {
+    const valueIsValid = (_value.length === 9) &&
+      allowedFirstHalf.test(firstHalf) && allowedLastHalf.test(lastHalf);
+    onChange(valueIsValid ? _value : '');
+  }, [_value]);
+
+  const disableLastHalf = !(conditionMet && allowedFirstHalf.test(firstHalf)) && (firstHalf.length < 4);
+  const error = !(allowedFirstHalf.test(firstHalf) && allowedLastHalf.test(lastHalf) && conditionMet) ? null : (_value.length < 9 ? 'ID must have 8 characters' : null);
 
   return (
     <>
       <Text
         style={[
-          error ? { color: '#b20008' } : {},
+          (allowedFirstHalf.test(firstHalf) && allowedLastHalf.test(lastHalf) && !error) ? {} : { color: '#b20008' },
           !conditionMet ? { color: '#999' } : {},
         ]}
       >{field.label}</Text>
 
-      <View style={[{ flexDirection: 'row', alignItems: 'center' }]}>
+      <View style={[{ flexDirection: 'row', alignItems: 'flex-start' }]}>
         <View style={[{ flex: 1 }]}>
           <Form>
-            <Item regular error={error ? true : false}>
+            <Item error={!allowedFirstHalf.test(firstHalf) ? true : false} disabled={!conditionMet}>
               <Input
+                ref={firstHalfRef}
+                maxLength={4}
                 autoCapitalize="characters"
                 editable={conditionMet}
-                value={value || ''}
-                defaultValue={value || ''}
+                value={firstHalf}
+                placeholder="ABC2"
                 onChange={e => {
                   const value = e.nativeEvent.text;
-                  onChange(value);
+                  setFirstHalf(value);
                 }}
                 // placeholder={field.label}
                 label={`${field.label}${field.optional ? '' : ' *'}`}
               />
             </Item>
           </Form>
+
+          {allowedFirstHalf.test(firstHalf) ? null : (
+            <>
+              <Text style={{ color: '#b20008' }}>
+                Allowed characters: ABCDEF
+              </Text>
+            </>
+          )}
         </View>
 
-        <Text style={[{ marginHorizontal: 10 }]}>-</Text>
+        <Text style={[{ marginHorizontal: 10, marginTop: 'auto', marginBottom: 'auto' }]}>-</Text>
 
         <View style={[{ flex: 1 }]}>
           <Form>
-            <Item regular error={error ? true : false}>
+            <Item error={!allowedLastHalf.test(lastHalf) ? true : false} disabled={disableLastHalf}>
               <Input
-                autoCapitalize="characters"
-                editable={conditionMet}
-                value={value || ''}
-                defaultValue={value || ''}
+                ref={lastHalfRef}
+                onKeyPress={e => {
+                  if (e.nativeEvent.key === 'Backspace' && !lastHalf) {
+                    setFirstHalf(firstHalf.substr(0, firstHalf.length - 1));
+                    firstHalfRef.current._root.focus();
+                  }
+                }}
+                maxLength={4}
+                keyboardType="numeric"
+                disabled={disableLastHalf}
+                editable={!disableLastHalf}
+                value={lastHalf}
+                placeholder="0123"
                 onChange={e => {
                   const value = e.nativeEvent.text;
-                  onChange(value);
+                  setLastHalf(value);
                 }}
                 // placeholder={field.label}
                 label={`${field.label}${field.optional ? '' : ' *'}`}
               />
             </Item>
           </Form>
+
+          {allowedLastHalf.test(lastHalf) ? null : (
+            <>
+              <Text style={{ color: '#b20008' }}>
+                Allowed characters: 0123456789
+              </Text>
+            </>
+          )}
         </View>
       </View>
 
