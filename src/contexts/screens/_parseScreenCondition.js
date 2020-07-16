bc902244ba5d2eddb888e0a1712bc401e08c4afd
farai <farai@bwsonline.co.za>
2020-07-16 13:44:08 +0200

    Screen condition parser

--- src/contexts/screens/_parseScreenCondition.js
+++ src/contexts/screens/_parseScreenCondition.js
@@ -3,47 +3,57 @@ const sanitizeCondition = (condition = '') => condition
   .replace(new RegExp(' or ', 'gi'), ' || ')
   .replace(new RegExp(' = ', 'gi'), ' == ');
 
-export default function parseScreenCondition({ state: { form: f, configuration } }) { 
-  return (_condition = '', form = f) => {
-    const parseConditionString = (s = '', key, value) => !s ? '' : s
-    .split(`$${key} =`).join(`${value} =`)
-    .split(`$${key}=`).join(`${value} =`)
-    .split(`$${key} >`).join(`${value} >`)
-    .split(`$${key}>`).join(`${value} >`)
-    .split(`$${key} <`).join(`${value} <`)
-    .split(`$${key}<`).join(`${value} <`);
+export const _parseScreenCondition = (_condition = '', opts = {}) => {
+  const { form, configuration } = opts;
 
-    const parseForm = (_condition = '', form) => {
-      const values = form.reduce((acc, entry) => [...acc, ...entry.values], []);
-      const chunks = values.map(({ value, type, key, dataType }) => {
-        value = value || '';
-        const t = dataType || type;
+  const parseConditionString = (s = '', key, value) => !s ? '' : s
+  .split(`$${key} =`).join(`${value} =`)
+  .split(`$${key}=`).join(`${value} =`)
+  .split(`$${key} >`).join(`${value} >`)
+  .split(`$${key}>`).join(`${value} >`)
+  .split(`$${key} <`).join(`${value} <`)
+  .split(`$${key}<`).join(`${value} <`);
 
-        switch(t) {
-          case 'number':
-            value = value || null;
-            break;
-          case 'boolean':
-            value = value || false;
-            break;
-          default:
-            value = JSON.stringify(value);
-        }
+  const parseForm = (_condition = '', form) => {
+    const values = form.reduce((acc, entry) => [...acc, ...entry.values], []);
+    const chunks = values.map(({ value, type, key, dataType }) => {
+      value = value || '';
+      const t = dataType || type;
 
-        return parseConditionString(_condition, key, value);
-      });
-      return chunks
-        .filter(c => c !== _condition)
-        .map(c => `(${c})`)
-        .join(' || ') || _condition;
-    };
+      switch(t) {
+        case 'number':
+          value = value || null;
+          break;
+        case 'boolean':
+          value = value || false;
+          break;
+        default:
+          value = JSON.stringify(value);
+      }
 
-    let condition = form ? parseForm(_condition, form) : _condition;
-    condition = parseForm(condition, f);
+      return parseConditionString(_condition, key, value);
+    });
+    return chunks
+      .filter(c => c !== _condition)
+      .map(c => `(${c})`)
+      .join(' || ') || _condition;
+  };
+
+  let condition = form ? parseForm(_condition, form) : _condition;
+
+  if (configuration) {
     condition = Object.keys(configuration).reduce((acc, key) => {
       return parseConditionString(acc, key, configuration[key] ? true : false);
     }, condition);
+  }
+  
+  return sanitizeCondition(condition);
+};
 
-    return sanitizeCondition(condition);
-  };
+export default function parseScreenCondition({ state: { form: f, configuration } }) { 
+  return (_condition = '', form) => {
+    let condition = form ? _parseScreenCondition(_condition, { form, configuration }) : _condition;
+    condition = _parseScreenCondition(condition, { form: f, configuration });
+    return condition;
+  }
 }
