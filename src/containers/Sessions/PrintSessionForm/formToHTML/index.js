8c3cf07bae5b31f29d9ce287873b22bb4820313d
farai <farai@bwsonline.co.za>
2020-07-27 13:10:54 +0200

    Print section display

--- src/containers/Sessions/PrintSessionForm/formToHTML/index.js
+++ src/containers/Sessions/PrintSessionForm/formToHTML/index.js
@@ -1,23 +1,39 @@
-import parseForm from './parseForm';
+/* eslint-disable indent */
+import ucFirst from '@/utils/ucFirst';
 import baseHTML from './baseHTML';
 
 export default form => {
-  const entries = parseForm(form);
-  const tables = entries.map(({ data, screen }) => {
+  const sections = [];
+  form.forEach(entry => {
+    const { screen: { sectionTitle } } = entry;
+    const _sectionTitle = ucFirst(`${sectionTitle}`.toLowerCase());
+    let sectionIndex = sections.map(([section]) => section).indexOf(_sectionTitle);
+    if (sectionIndex < 0) {
+      sections.push([sectionTitle ? _sectionTitle : '', []]);
+      sectionIndex = sections.length - 1;
+    }
+    sections[sectionIndex][1].push(entry);
+  });
+
+  const tables = sections.map(([sectionTitle, entries]) => {
+    entries = entries.filter(e => e.values.length);
+
     return `
-      <div class="form-table-wrapper">
-        <div class="form-table-title">
-          <strong>${screen.title}</strong>
+      ${!sectionTitle ? '' : (`
+        <div class="title row">
+          <strong>${sectionTitle}</strong>
         </div>
-        <table class="form-table">
-          ${data.map(entry => `
-            <tr>
-              <td>${entry.label}</td>
-              <td>${entry.values.map(v => `<p>${v.text || v.valueText || v.label || v.value}</p>`).join('')}</td>
-            </tr>
-          `)}
-        </table>
-      </div>
+      `)}
+
+      ${entries.filter(e => e.values.length)
+        .map(({ values, screen: { metadata: { label } } }) =>
+          values.map(v => `
+            <div  class="row">
+              <span>${label || v.label}</span>
+              <span>${v.valueText || 'N/A'}</span>
+            </div>
+          `).join('')
+        ).join('')}
     `;
   }).join('');
 
