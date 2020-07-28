5846ab18d59c78f9bb35b94f430c9df1ee3b3f90
farai <farai@bwsonline.co.za>
2020-07-28 16:03:57 +0200

    Export

--- src/contexts/sessions/ContextValue/export/index.js
+++ src/contexts/sessions/ContextValue/export/index.js
@@ -3,6 +3,7 @@ import * as FileSystem from 'expo-file-system';
 import * as Permissions from 'expo-permissions';
 import * as MediaLibrary from 'expo-media-library';
 import { Alert } from 'react-native';
+import { exportSession } from '@/api/export';
 
 const exportSuccessAlert = (msg = '') => {
   Alert.alert(
@@ -18,7 +19,7 @@ const exportSuccessAlert = (msg = '') => {
   );
 };
 
-function exportJSON() {
+export function exportJSON() {
   const saveFile = async () => {
     const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
     if (status === 'granted') {
@@ -35,7 +36,7 @@ function exportJSON() {
   saveFile();
 }
 
-function exportEXCEL() {
+export function exportEXCEL() {
   const data = [
     { name: 'John', city: 'Seattle', },
     { name: 'Mike', city: 'Los Angeles', },
@@ -67,18 +68,44 @@ function exportEXCEL() {
   // writeFile(file, wbout, 'ascii').then(console.log).catch(console.log);
 }
 
-function exportToApi() {
+export function exportToApi() {
+  const sessions = this.state.sessions.filter(s => !s.exported)
+    .map(s => {
+      const { script, form } = s.data;
+
+      return {
+        script: { id: script.id, title: script.data.title },
+        entries: form.reduce((acc, e) => {
+          return [
+            ...acc,
+            ...e.values.map(({ key, dataType, value, label, }) => ({
+              key,
+              type: dataType,
+              values: [{ value, label }],
+            }))
+          ];
+        }, []),
+      };
+    });
+
+  const postData = sessions.map(data => ({
+    uid: `${this.uid_prefix}-0001`,
+    scriptId: data.script.id,
+    data,
+  }));
+
   this.setState({ exporting: true });
+
+  Promise.all(postData.filter((s, i) => i === 0).map(s => exportSession(s)))
+    .then(rslts => {
+      console.log('SUCCESS', rslts);
+    })
+    .catch(e => {
+      console.log('ERROR', e);
+    });
+
   setTimeout(() => {
     this.setState({ exporting: false });
     exportSuccessAlert('Export success');
   }, 2000);
-}
-
-export default function _export() {
-  return {
-    exportJSON: exportJSON.bind(this),
-    exportEXCEL: exportEXCEL.bind(this),
-    exportToApi: exportToApi.bind(this),
-  };
 }
