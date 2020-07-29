f97725c90090b5a0a1cd4dd2a6f0be8e4845f895
farai <farai@bwsonline.co.za>
2020-07-29 12:04:50 +0200

    onScreenLocationChange

--- src/contexts/screens/ContextValue/_onLocationChange.js
+++ src/contexts/screens/ContextValue/_onLocationChange.js
@@ -1,30 +1,41 @@
 export default function onLocationChange() {
   const { screenId } = this.router.match.params;
-  const { screensInitialised, screens } = this.state;
+  const { screensInitialised, } = this.state;
 
   if (screensInitialised) {
-    const activeScreenIndex = screenId ? screens.map(s => s.id.toString()).indexOf(screenId) : 0;
-    const activeScreen = screens[activeScreenIndex];
-    this.setState({
-      activeScreenIndex,
-      activeScreenInitialised: true,
-      activeScreen: !activeScreen ? null : {
-        ...activeScreen,
-        data: {
-          ...activeScreen.data,
-          metadata: {
-            ...activeScreen.data.metadata,
-            ...(activeScreen.data.metadata || {}).items ?
-              { items: activeScreen.data.metadata.items.sort((a, b) => a.position - b.position) }
-              :
-              null,
-            ...(activeScreen.data.metadata || {}).fields ?
-              { fields: activeScreen.data.metadata.fields.sort((a, b) => a.position - b.position) }
-              :
-              null
+    this.setState(({ screens, form, cachedForm, activeScreenIndex, }) => {
+      const targetScreenIndex = screenId ? screens.map(s => s.id.toString()).indexOf(screenId) : 0;
+      const targetScreen = screens[targetScreenIndex];
+
+      return {
+        cachedForm: [
+          ...cachedForm.filter(cachedEntry => !form.map(entry => entry.screen.id).includes(cachedEntry.screen.id)),
+          ...form
+        ],
+        activeScreenIndex: targetScreenIndex,
+        activeScreenInitialised: true,
+        form: form.reduce((acc, e, i) => {
+          if ((targetScreenIndex < activeScreenIndex) && (i === (form.length - 1))) return acc;
+          return [...acc, e];
+        }, []),
+        activeScreen: !targetScreen ? null : {
+          ...targetScreen,
+          data: {
+            ...targetScreen.data,
+            metadata: {
+              ...targetScreen.data.metadata,
+              ...(targetScreen.data.metadata || {}).items ?
+                { items: targetScreen.data.metadata.items.sort((a, b) => a.position - b.position) }
+                :
+                null,
+              ...(targetScreen.data.metadata || {}).fields ?
+                { fields: targetScreen.data.metadata.fields.sort((a, b) => a.position - b.position) }
+                :
+                null
+            }
           }
-        }
-      },
+        },
+      };
     });
   }
 }
