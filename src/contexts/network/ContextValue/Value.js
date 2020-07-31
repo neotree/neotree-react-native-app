516aa416b111856ada39cf3ac0c665392ff9288a
farai <farai@bwsonline.co.za>
2020-07-31 10:12:32 +0200

    Context value

--- src/contexts/network/ContextValue/Value.js
+++ src/contexts/network/ContextValue/Value.js
@@ -5,13 +5,11 @@ export { defaults };
 export default class ContextValue {
   constructor(params) {
     this.defaults = defaults;
-    this.init(params);
+    require('./_init').default.bind(this)(params);
   }
 
   setState = s => this._setState(prevState => ({
     ...prevState,
     ...(typeof s === 'function' ? s(prevState) : s)
   }));
-
-  init = require('./_init').default.bind(this);
 }
