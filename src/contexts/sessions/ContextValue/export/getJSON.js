7f723672e04b98778b226b0a9aa702a4c803b5f1
farai <farai@bwsonline.co.za>
2020-09-03 12:51:45 +0200

    Dropdown json export

--- src/contexts/sessions/ContextValue/export/getJSON.js
+++ src/contexts/sessions/ContextValue/export/getJSON.js
@@ -29,14 +29,14 @@ export default function getJSON(_sessions = [], opts = {}) {
           return [
             ...acc,
             ...e.values.map(v => {
-              const { key, type, dataType, value, label, } = v;
+              const { key, type, dataType, value, label, valueLabel, } = v;
               return {
                 key,
                 type: dataType || type,
                 values: value && value.map ? 
-                  value.map(({ value, label, }) => ({ value, label })) 
+                  value.map(({ value, label, valueLabel, }) => ({ value, label: valueLabel || label, })) 
                   : 
-                  [{ value: getVal(v), label }],
+                  [{ value: getVal(v), label: valueLabel || label, }],
               };
             })
           ];
