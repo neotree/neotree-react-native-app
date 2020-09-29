97aebf86ea607ad6618984ac388740e71284d1cb
farai <farai@bwsonline.co.za>
2020-09-29 12:46:52 +0200

    Progress checkmark styles

--- src/containers/Script/Screens/ScreenType/TypeProgress.js
+++ src/containers/Script/Screens/ScreenType/TypeProgress.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { View } from 'react-native';
 import Text from '@/components/Text';
 import { Icon } from 'native-base';
+import theme from '@/native-base-theme/variables/commonColor';
 
 const Progress = ({ screen, onChange }) => {
   const metadata = screen.data.metadata || {};
@@ -20,23 +21,49 @@ const Progress = ({ screen, onChange }) => {
             <View
               key={key}
               style={{
-                alignItems: 'center',
                 flexDirection: 'row',
-                padding: 10,
               }}
             >
-              <View style={{ marginRight: 10 }}>
-                <Icon
-                  style={{
-                    fontSize: 40,
-                    color: item.checked ? 'green' : '#ccc'
+              <View 
+                style={{ 
+                  marginRight: 10,
+                  alignItems: 'center',
+                }}
+              >
+                <View
+                  style={{ 
+                    width: 36,
+                    height: 36,
+                    borderWidth: 2,
+                    borderColor: item.checked ? 'green' : '#999',
+                    borderRadius: 18,
+                    alignItems: 'center',
+                    justifyContent: 'center',
                   }}
-                  name="checkmark-circle-outline"
-                />
+                >
+                  <Icon
+                    style={{
+                      fontSize: 30,
+                      color: item.checked ? 'green' : '#ccc'
+                    }}
+                    name="checkmark"
+                  />
+                </View>
+
+                {i < (metadata.items.length - 1) && (
+                  <View
+                    style={{ 
+                      width: 2,
+                      minHeight: 20,
+                      backgroundColor: '#ccc',
+                      flex: 1,
+                    }}
+                  />
+                )}
               </View>
 
-              <View style={{ flex: 1, }}>
-                <Text>{item.label}</Text>
+              <View style={{ flex: 1, marginTop: 2 }}>
+                <Text style={{ fontSize: 22 }}>{item.label}</Text>
               </View>
             </View>
           );
