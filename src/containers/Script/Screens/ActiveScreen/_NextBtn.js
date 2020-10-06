fdcccbec2a648c358c08e3494957ab09b23edd47
farai <farai@bwsonline.co.za>
2020-10-06 09:51:50 +0200

    Summary page

--- src/containers/Script/Screens/ActiveScreen/_NextBtn.js
+++ src/containers/Script/Screens/ActiveScreen/_NextBtn.js
@@ -2,21 +2,37 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useScreensContext } from '@/contexts/screens';
 import { Icon } from 'native-base';
-import { TouchableOpacity } from 'react-native';
+import { TouchableOpacity, View, Modal } from 'react-native';
 import theme from '@/native-base-theme/variables/commonColor';
+import { Spinner } from 'native-base';
+import { useLocation } from 'react-router-native';
 
 const NextBtn = ({ onNext, }) => {
-  const {
-    goToSummary,
-    canSave,
-    goToScreen,
-  } = useScreensContext();
+  const { location } = useLocation();
+  const { canSave, goToScreen, goToSummary, } = useScreensContext();
+  const [displayLoader, setDisplayLoader] = React.useState(false);
+
+  React.useEffect(() => { setDisplayLoader(false); }, [location]);
+
+  if (displayLoader) {
+    return (
+      <Modal open>
+        <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1, }}>
+          <Spinner color={theme.brandInfo} />
+        </View>
+      </Modal>
+    );
+  }
 
   return (
     <>
       <TouchableOpacity
         onPress={() => {
-          if (canSave()) return goToSummary();
+          if (canSave()) {
+            setDisplayLoader(true);
+            setTimeout(() => goToSummary(), 0);
+            return;
+          };
           goToScreen('next');
           if (onNext) onNext();
         }}
