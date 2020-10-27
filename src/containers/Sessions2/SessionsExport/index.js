204f62f2fd86a1e328b842c290ff5ed7de626e99
farai <farai@bwsonline.co.za>
2020-10-27 18:00:42 +0200

    0.4.37-DEV

---
+++ src/containers/Sessions2/SessionsExport/index.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { useSessionsContext } from '@/contexts/sessions';
+import { ListItem, Right, Left, Radio, Button, Card, } from 'native-base';
+import { View } from 'react-native';
+import Text from '@/components/Text';
+import Content from '@/components/Content';
+import Divider from '@/components/Divider';
+import OverlayLoader from '@/components/OverlayLoader';
+import Header from './Header';
+
+const ExportPage = () => {
+  const { exportJSON, exportEXCEL, exportToApi, state: { exporting } } = useSessionsContext();
+
+  const [format, setFormat] = React.useState('excel');
+
+  const opts = [
+    { label: 'Excel Spreadsheet', value: 'excel' },
+    { label: 'JSON', value: 'json' },
+    { label: 'JSONAPI', value: 'jsonapi' },
+  ];
+
+  return (
+    <>
+      <Header />
+
+      <Content containerProps={{ style: { flex: 1 } }}>
+        <View 
+          style={{ 
+            padding: 20,
+          }}
+        >
+          {opts.map(opt => (
+            <ListItem
+              key={opt.value}
+              selected={format === opt.value}
+              onPress={() => setFormat(opt.value)}
+            >
+              <Left>
+                <Text>{opt.label}</Text>
+              </Left>
+              <Right>
+                <Radio
+                  selected={format === opt.value}
+                  onPress={() => setFormat(opt.value)}
+                />
+              </Right>
+            </ListItem>
+          ))}
+
+          <Divider border={false} spacing={2} />
+
+          <View style={{ alignItems: 'flex-end' }}>
+            <Button
+              disabled={!format}
+              onPress={() => {
+                if (format === 'excel') return exportEXCEL();
+                if (format === 'json') return exportJSON();
+                if (format === 'jsonapi') return exportToApi();
+              }}
+            >
+              <Text>Export</Text>
+            </Button>
+          </View>
+        </View>
+      </Content>
+
+      <OverlayLoader display={exporting} />
+    </>
+  );
+};
+
+export default ExportPage;
