d494821700a8ed8906cc197b077cb6487daaecbc
farai <farai.matare@bwscloud.tech>
2021-03-17 11:59:44 +0200

    Refactor

---
+++ src/api/database/configuration/_saveConfiguration.js
@@ -0,0 +1,19 @@
+import db from '../db';
+
+export default (data = {}) => new Promise((resolve, reject) => {
+  db.transaction(
+    tx => {
+      tx.executeSql(
+        'insert or replace into configuration (id, data, createdAt, updatedAt) values (?, ?, ?, ?);',
+        [1, JSON.stringify(data || {}), new Date().toISOString(), new Date().toISOString()],
+        (tx, rslts) => resolve(rslts),
+        (tx, e) => {
+          if (e) {
+            require('@/utils/logger')('ERROR: saveConfiguration', e);
+            reject(e);
+          }
+        }
+      );
+    }
+  );
+});
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Image, View } from 'react-native';
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList, DrawerItem } from '@react-navigation/drawer';
 import { createStackNavigator } from '@react-navigation/stack';
 import LazComponent from '@/components/LazyComponent';
-import * as SCREENS from '@/constants/SCREENS';
+import Logo from '@/components/Logo';
 import { useAppContext } from '@/AppContext';
+import theme from '~/native-base-theme/variables/commonColor';
 
 const Authentication = LazComponent(() => import('./Authentication'));
 const Location = LazComponent(() => import('./Location'));
@@ -18,10 +20,41 @@ const Drawer = createDrawerNavigator();
 function Home() {
   return (
     <>
-      <Drawer.Navigator initialRouteName="Scripts">
-        <Drawer.Screen name={SCREENS.SCRIPTS.name} component={Scripts} />
-        <Drawer.Screen name={SCREENS.CONFIGURATION.name} component={Configuration} />
-        <Drawer.Screen name={SCREENS.SESSIONS.name} component={Sessions} />
+      <Drawer.Navigator
+        initialRouteName="Scripts"
+        drawerStyle={{ margin: 0, padding: 0, }}
+        drawerContent={props => {
+          const { navigation: { navigate } } = props; // eslint-disable-line
+          return (
+            <DrawerContentScrollView
+              {...props}
+            >
+              <View
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  margin: 0,
+                  height: 200,
+                  backgroundColor: theme.buttonPrimaryBg,
+                  padding: 0,
+                  position: 'absolute',
+                  top: 0,
+                  width: '100%'
+                }}
+              ><Logo /></View>
+
+              <View style={{ height: 200, marginBottom: 10, }} />
+
+              <DrawerItemList {...props} />
+              <DrawerItem label="Configuration" onPress={() => navigate('Configuration')} />
+              <DrawerItem label="Sessions history" onPress={() => navigate('Sessions')} />
+              <DrawerItem label="Sign out" onPress={() => alert('Link to help')} />
+            </DrawerContentScrollView>
+          );
+        }}
+      >
+        <Drawer.Screen name="Scripts" component={Scripts} />
       </Drawer.Navigator>
     </>
   );
@@ -46,11 +79,13 @@ function Containers() {
         // }}
       >
         <Stack.Screen
-          name={SCREENS.HOME.name}
+          name="Home"
           component={Home}
           options={{ headerShown: false }}
         />
-        <Stack.Screen name={SCREENS.SCRIPT.name} component={Script} />
+        <Stack.Screen name="Configuration" component={Configuration} />
+        <Stack.Screen name="Sessions" component={Sessions} />
+        <Stack.Screen name="Script" component={Script} />
       </Stack.Navigator>
     </>
   );
