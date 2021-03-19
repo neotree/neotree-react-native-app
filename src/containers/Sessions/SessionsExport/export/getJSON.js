b3aa5bb55d6e59837833f73c618ac210bdf43f8b
farai <farai@bwsonline.co.za>
2021-03-19 12:32:43 +0200

    Fix data export

--- src/containers/Sessions/SessionsExport/export/getJSON.js
+++ src/containers/Sessions/SessionsExport/export/getJSON.js
@@ -4,7 +4,7 @@ export default function getJSON(opts = {}) {
   const { showConfidential, sessions: _sessions, application } = opts;
 
   const sessions = (_sessions || []).map((s) => {
-    const { script, form } = s.data;
+    const { script, form, app_mode, country, hospital_id } = s.data;
 
     const data = {
       uid: s.uid,
@@ -12,6 +12,9 @@ export default function getJSON(opts = {}) {
       scriptVersion: application.webeditor_info.version,
       scriptTitle: script.script_id,
       script: { id: script.script_id, title: script.data.title },
+      app_mode,
+      country,
+      hospital_id,
       entries: form
         .map((e) => ({
           ...e,
