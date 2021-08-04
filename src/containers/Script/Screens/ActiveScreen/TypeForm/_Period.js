b17d99f56551aef7290506b87e6b4027614ce288
farai <farai@bwsonline.co.za>
2021-08-04 14:04:03 +0200

    Form field: Time period

--- src/containers/Script/Screens/ActiveScreen/TypeForm/_Period.js
+++ src/containers/Script/Screens/ActiveScreen/TypeForm/_Period.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import DatePicker from '@/components/DatePicker';
-import formCopy from '@/constants/copy/form';
+import { View, TouchableOpacity } from 'react-native';
+import Text from '@/components/Text';
 import moment from 'moment';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { MaterialIcons } from '@expo/vector-icons';
 
 const formatDate = d => {
   if (!d) return '';
@@ -15,44 +17,94 @@ const formatDate = d => {
   return v.filter(s => s).join(', ');
 };
 
-const _Period = ({ form, field, value, onChange: _onChange, conditionMet, }) => {
-  const onChange = (d, error) => _onChange(d, {
-    error,
-    valueText: d ? formatDate(d) : null,
-    exportValue: d ? formatDate(d) : null,
-  });
-  const calcFrom = form.values.filter(v => `$${v.key}` === field.calculation)[0];
-  const [date, setDate] = React.useState(field.defaultValue ? value || new Date() : value);
-
-  React.useEffect(() => {
-    if (calcFrom) onChange(calcFrom.value);
-  }, [calcFrom, value]);
-
-  const onDateChange = (e, date) => {
-    setDate(date);
-    onChange(date.toISOString());
+const _Period = ({ form, field, onChange: _onChange, conditionMet, valueObject, }) => {
+  const onChange = (d, error) => {
+    _onChange(d, {
+      error,
+      value: d ? ((new Date().getTime() - new Date(d).getTime()) / (1000 * 60 * 60)) : null,
+      valueText: d ? formatDate(d) : null,
+      exportValue: d ? formatDate(d) : null,
+    });
   };
 
-  React.useEffect(() => {
-    if (!calcFrom) {
-      const v = value ? new Date(value).toISOString() : null;
-      const d = date ? new Date(date).toISOString() : null;
-      if (v !== d) onChange(date);
-    }
-  });
+  const [showDatePicker, setShowDatePicker] = React.useState(false);
+  const [dateTimePickerMode, setDateTimePickerMode] = React.useState('date');
+  const [date, setDate] = React.useState(null);
+  const [dateNoTime, setDateNoTime] = React.useState(null);
+
+  const calcFrom = form.values.filter(v => `$${v.key}` === field.calculation)[0];
+
+  React.useEffect(() => { if (calcFrom) onChange(calcFrom.value); }, [calcFrom]);
+
+  React.useEffect(() => { if (!calcFrom && date) onChange(date); }, [calcFrom, date]);
 
   return (
     <>
-      <DatePicker
-        enabled={conditionMet}
-        editable={!calcFrom}
-        value={calcFrom ? value : date}
-        placeholder="Select" // {field.calculation || formCopy.SELECT_DATE}
-        onChange={calcFrom ? (() => {}) : onDateChange}
-        formatDate={formatDate}
+      <Text
+        style={conditionMet ? {} : { color: '#999' }}
+      >{field.label}{field.optional ? '' : ' *'}</Text>
+
+      <View
+        style={{
+          padding: 10,
+          borderColor: '#ccc',
+          borderWidth: 1,
+          flexDirection: 'row',
+          alignItems: 'center',
+        }}
       >
-        {field.label}{field.optional ? '' : ' *'}
-      </DatePicker>
+        <Text
+          variant="caption"
+          style={({ flex: 1, ...(conditionMet ? {} : { color: '#999' }) })}
+        >{valueObject ? valueObject.valueText : ''}</Text>
+
+        {!calcFrom && (
+          <TouchableOpacity
+            disabled={!conditionMet}
+            onPress={() => setShowDatePicker(true)}
+          >
+            <MaterialIcons
+              size={24}
+              color="black"
+              style={conditionMet ? {} : { color: '#999' }}
+              name="event"
+            />
+          </TouchableOpacity>
+        )}
+      </View>
+
+      {showDatePicker && (
+        <View style={{ flex: 1 }}>
+          <DateTimePicker
+            mode={dateTimePickerMode}
+            testID="periodDateTimePicker"
+            timeZoneOffsetInMinutes={0}
+            value={date || new Date()}
+            is24Hour
+            display="default"
+            maximumDate={new Date()}
+            onChange={(e, newDate) => {
+              setShowDatePicker(false);
+              if (dateTimePickerMode === 'time') {
+                if (dateNoTime && newDate) {
+                  const newestDate = new Date(dateNoTime);
+                  newestDate.setHours(newDate.getHours());
+                  newestDate.setMinutes(newDate.getMinutes());
+                  newDate = newestDate;
+                }
+                setDateTimePickerMode('date');
+                setDate(newDate);
+                setShowDatePicker(false);
+              } else {
+                setDateNoTime(newDate);
+                setDateTimePickerMode('time');
+                setDate(newDate);
+                setShowDatePicker(true);
+              }
+            }}
+          />
+        </View>
+      )}
     </>
   );
 };
@@ -62,7 +114,8 @@ _Period.propTypes = {
   field: PropTypes.object.isRequired,
   onChange: PropTypes.func.isRequired,
   conditionMet: PropTypes.bool,
-  value: PropTypes.any,
+  // value: PropTypes.any,
+  valueObject: PropTypes.object,
 };
 
 export default _Period;
