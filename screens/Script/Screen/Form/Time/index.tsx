4f7e7b46fd4eacb7b79001b01323644ad9c2e569
farai <farai@bwsonline.co.za>
2021-11-12 13:44:38 +0200

    Time field

--- screens/Script/Screen/Form/Time/index.tsx
+++ screens/Script/Screen/Form/Time/index.tsx
@@ -1,22 +1,40 @@
 import React from 'react';
-import { useTheme } from '@/components/ui';
 import { DatePicker } from '@/components/DatePicker';
 import * as copy from '@/constants/copy/script';
-import { useScriptContext } from '../../../Context';
 import { ScreenFormFieldComponentProps } from '../../../types';
 
-export function Time({ field }: ScreenFormFieldComponentProps) {
-    const theme = useTheme();
-    const { activeScreen } = useScriptContext();
+export function Time({ 
+    field, 
+    onChange: _onChange, 
+    value, 
+    conditionMet, 
+}: ScreenFormFieldComponentProps) {
+    const onChange = (d, opts?: any) => _onChange(d, {
+        ...opts,
+        valueText: d ? require('moment')(new Date(d)).format('HH:mm') : '',
+    });
 
-    const [date, setDate] = React.useState(null);
+    const [date, setDate] = React.useState(field.defaultValue ? value || new Date() : value);
+
+    const onDateChange = (date) => {
+        setDate(date);
+        onChange(date);
+    };
+
+    React.useEffect(() => {
+        const v = value ? new Date(value).toISOString() : null;
+        const d = date ? new Date(date).toISOString() : null;
+        if (v !== d) onChange(date);
+    });
 
     return (
         <>
             <DatePicker
                 mode="time"
+                disabled={!conditionMet}
                 label={copy.SELECT_DATE_AND_TIME}
-                onChange={date => {}}
+                value={date || null}
+                onChange={date => onDateChange(date)}
             />
         </>
     );
