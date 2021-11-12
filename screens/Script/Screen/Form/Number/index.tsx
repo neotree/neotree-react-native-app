3ed45ff1b1a059033e248b66de977dce6c06129a
farai <farai@bwsonline.co.za>
2021-11-12 13:31:59 +0200

    Number field

--- screens/Script/Screen/Form/Number/index.tsx
+++ screens/Script/Screen/Form/Number/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { useTheme, View, TextField, Text } from '@/components/ui';
-import { useScriptContext } from '../../../Context';
+import { View, TextField } from '@/components/ui';
 import { ScreenFormFieldComponentProps } from '../../../types';
 
-export function FieldNumber({ field }: ScreenFormFieldComponentProps) {
-    const theme = useTheme();
-    const { activeScreen } = useScriptContext();
-
+export function FieldNumber({
+    field,
+    onChange,
+    value,
+    conditionMet,
+  }: ScreenFormFieldComponentProps) {
     const [error, setError] = React.useState(null);
 
     let maxDecimals = 0;
@@ -16,9 +17,38 @@ export function FieldNumber({ field }: ScreenFormFieldComponentProps) {
         <>
             <View>
                 <TextField 
+                    editable={conditionMet}
                     variant="outlined"
                     autoCapitalize="none"
                     keyboardType={maxDecimals ? 'decimal-pad' : 'numeric'}
+                    error={!!error}
+                    helperText={error}
+                    value={value || ''}
+                    defaultValue={value || ''}
+                    autoCorrect={false}
+                    onChange={e => {
+                        const value = e.nativeEvent.text;
+                        let err = null;
+                        if (value) {
+                            const v = Number(value);
+                            const decimals = value.split('.').filter((n, i) => i > 0).join('');
+                            if (value.indexOf(' ') > -1) {
+                                err = 'No spaces allowed'
+                            } else if (isNaN(v)) {
+                                err = 'Input is not a number';
+                            } else if (field.maxValue && (v > Number(field.maxValue))) {
+                                err = `Max value ${field.maxValue}`;
+                            } else if (field.minValue && (v < Number(field.minValue))) {
+                                err = `Min value ${field.minValue}`;
+                            } else if (maxDecimals && (decimals.length > maxDecimals)) {
+                                err = `Number should have only ${maxDecimals} decimal places.`;
+                            } else if (!maxDecimals && value.indexOf('.') > -1) {
+                                err = 'Decimal places not allowed.'
+                            }
+                        }
+                        setError(err);
+                        onChange(value, { error: err, valueText: value });
+                    }}
                 />
             </View>
         </>
