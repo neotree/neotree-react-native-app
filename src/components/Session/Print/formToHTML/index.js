f9c4ad70bde8fbb4913f5b57086a8bf7fd0aa746
farai <farai@bwsonline.co.za>
2021-11-16 14:06:45 +0200

    Diagnoses printout

--- src/components/Session/Print/formToHTML/index.js
+++ src/components/Session/Print/formToHTML/index.js
@@ -6,15 +6,40 @@ export default (session, showConfidential) => {
   const { form } = session.data;
   const sections = [];
   form.forEach(entry => {
-    const { screen: { sectionTitle } } = entry;
+    const { screen } = entry;
+    const { sectionTitle } = screen;
     if (entry.values.length) {
-      const _sectionTitle = ucFirst(`${sectionTitle}`.toLowerCase());
-      let sectionIndex = sections.map(([section]) => section).indexOf(_sectionTitle);
-      if (sectionIndex < 0) {
-        sections.push([sectionTitle ? _sectionTitle : '', []]);
-        sectionIndex = sections.length - 1;
-      }
-      sections[sectionIndex][1].push(entry);
+      [
+        ...(screen.type === 'diagnosis' ? [
+          `${sectionTitle} - Primary Provisional Diagnosis`,
+          `${sectionTitle} - Other problems`,
+        ] : [sectionTitle])
+      ].forEach((sectionTitle, i) => {
+        const _sectionTitle = ucFirst(`${sectionTitle}`.toLowerCase());
+        let sectionIndex = sections.map(([section]) => section).indexOf(_sectionTitle);
+        if (sectionIndex < 0) {
+          sections.push([sectionTitle ? _sectionTitle : '', []]);
+          sectionIndex = sections.length - 1;
+        }
+        sections[sectionIndex][1].push({
+          ...entry,
+          values: entry.values.filter(v => {
+            if (screen.type === 'diagnosis') {
+              let accepted = v.diagnosis.how_agree !== 'No';
+              if (!accepted) return false;
+              return i === 1 ? !v.diagnosis.isPrimaryProvisionalDiagnosis : v.diagnosis.isPrimaryProvisionalDiagnosis;
+            }
+            return true;
+          })
+        });
+      });
+      // const _sectionTitle = ucFirst(`${sectionTitle}`.toLowerCase());
+      // let sectionIndex = sections.map(([section]) => section).indexOf(_sectionTitle);
+      // if (sectionIndex < 0) {
+      //   sections.push([sectionTitle ? _sectionTitle : '', []]);
+      //   sectionIndex = sections.length - 1;
+      // }
+      // sections[sectionIndex][1].push(entry);
     }
   });
 
