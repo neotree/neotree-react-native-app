f9facc4f7aa3c1b9ec14b7dd5d036e7af81fa111
farai <=>
2023-01-04 11:20:36 +0200

    Progress screen

--- src/Home/Script/Screen/_TypeProgress.tsx
+++ src/Home/Script/Screen/_TypeProgress.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Box } from '../../../components';
+import Icon from '@expo/vector-icons/MaterialIcons';
+import { Box, useTheme, Text } from '../../../components';
 import { useContext } from '../Context';
 import * as types from '../../../types';
 
@@ -8,11 +9,35 @@ type TypeProgressProps = types.ScreenTypeProps & {
 };
 
 export function TypeProgress({}: TypeProgressProps) {
+    const theme = useTheme();
     const ctx = useContext();
+
+    const metadata = ctx?.activeScreen?.data?.metadata;
 
     return (
         <Box>
+            {metadata.items.map((item: any, i: number) => {
+                return (
+                    <React.Fragment key={i}>
+                        <Box 
+                            flexDirection="row"
+                            paddingVertical="m"
+                        >
+                            <Box>
+                                <Icon
+                                    name="check-circle-outline"
+                                    size={24}
+                                    color={theme.colors[item.checked ? 'success' : 'grey-400']}
+                                />
+                            </Box>
 
+                            <Box marginLeft="m">
+                                <Text variant="title3">{item.label}</Text>
+                            </Box>
+                        </Box>
+                    </React.Fragment>
+                )
+            })}
         </Box>
     );
 }
