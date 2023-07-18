d6035b49ac9d8943651ce1ed908b2d559432b704
mbaradza <mbaradza1@gmail.com>
2023-07-18 08:39:49 +0200

    Fix Error Saving Error

--- src/utils/handleCrashes.ts
+++ src/utils/handleCrashes.ts
@@ -7,18 +7,23 @@ export async function handleAppCrush(error: any) {
     console.log("====STACK===",stack)
     console.log("====MSG===",message)
     let app = await dbTransaction('select * from application where id=1;');
+    console.log("====MSG2===",app)
       let  application = app[0];
         if(application){
+           
             let e = await dbTransaction(`select * from exceptions where message='${message}'`);
-            if(e){
+            
+            if(e && e.length>0){
+                console.log("====MSGE3===",e)
                 // DO NOTHING ERROR ALREADY CAPTURED
             }else{
+                
                 //RECORD THE ERROR
                 try{
                 const columns = ['message', 'stack', 'device', 'exported','country','hospital'].join(',');
                 const values = ['?', '?', '?', '?', '?','?'].join(',');
                 const location = await getLocation();
-
+                console.log("====MSGE4===",location)
                 await dbTransaction(`insert into exceptions (${columns}) values (${values});`, [
                     message,
                     stack,
@@ -27,7 +32,9 @@ export async function handleAppCrush(error: any) {
                     location?.country,
                     location?.hospital
                 
-                ]);
+                ]).then(res=>{
+                    console.log("===ZVAITA====",res)
+                });
             }catch(ex){
             console.log("---DB EX---",ex)
             }
