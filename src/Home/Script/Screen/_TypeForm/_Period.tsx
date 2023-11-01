a16044c1e0f5c77a1cf096cc3b0cb874274dd1ce
farai-bws <farai.matare@bwscloud.tech>
2023-11-01 08:12:22 +0200

    2.1.5

--- src/Home/Script/Screen/_TypeForm/_Period.tsx
+++ src/Home/Script/Screen/_TypeForm/_Period.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
 import { Box, DatePicker } from '../../../../components';
 import * as types from '../../../../types';
-import { diffHours } from '../../../../utils/diffHours';
+import { diffHours, diffHours2 } from '../../../../utils/diffHours';
 
 type PeriodFieldProps = types.ScreenFormTypeProps & {
     
 };
 
 function dateToValueText(value: null | Date) {
-    let _value = value ? diffHours(new Date(value), new Date()) : null;
-    _value = value ? ((_value || 0) < 1 ? 1 : _value) : null;
-    let days = 0;
-    let hrs = 0;
-    let valueText = [];
-    if (_value) {
-        days = Math.floor(_value / 24);
-        hrs = Math.floor(_value % 24);
-        if (days) valueText.push(`${days} day(s)`);
-        if (hrs) valueText.push(`${hrs} hour(s)`);
-    }
-    return value ? valueText.map(t => t).join(', ') : '';
+	return value ? diffHours2(Math.abs((new Date().getTime() - new Date(value).getTime()) / 1000)) : null;
+    // let _value = value ? diffHours(new Date(value), new Date()) : null;
+    // _value = value ? ((_value || 0) < 1 ? 1 : _value) : null;
+    // let days = 0;
+    // let hrs = 0;
+    // let valueText = [];
+    // if (_value) {
+    //     days = Math.floor(_value / 24);
+    //     hrs = Math.floor(_value % 24);
+    //     if (days) valueText.push(`${days} day(s)`);
+    //     if (hrs) valueText.push(`${hrs} hour(s)`);
+    // }
+    // return value ? valueText.map(t => t).join(', ') : '';
 }
 
 export function PeriodField({ field, conditionMet, onChange, entryValue, formValues }: PeriodFieldProps) {
