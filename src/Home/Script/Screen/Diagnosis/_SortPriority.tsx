55b920d411b72be911ba27c41b70d797c325cbca
farai <lafarai.dev@gmail.com>
2024-05-22 20:51:56 +0200

    scroll diagnoses

--- src/Home/Script/Screen/Diagnosis/_SortPriority.tsx
+++ src/Home/Script/Screen/Diagnosis/_SortPriority.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ScrollView } from 'react-native';
 import { Box, Dropdown, DropdownOption } from '../../../../components';
 import * as types from '../../../../types';
 import { DiagnosesList } from './components/DiagnosesList';
@@ -20,80 +21,82 @@ export function SortPriority(props: SortPriorityProps) {
       }, [diagnoses, mounted]);
 
     return (
-        <Box>
-            <DiagnosesList
-                {...props}
-                divider
-                canAgreeDisagree={false}
-                canDelete={false}
-                title="Compiled Diagnoses"
-                subtitle="Please order the diagnoses by priority"
-                filter={filterCompiled}
-                itemWrapper={((card, { item: d, index: i }) => {
-                    // const onChange = (value: any) => setDiagnoses(diagnoses.map((d, index) => index !== i ? d : {
-                    //     ...d,
-                    //     ...value,
-                    // }));
+        <ScrollView>
+            <Box>
+                <DiagnosesList
+                    {...props}
+                    divider
+                    canAgreeDisagree={false}
+                    canDelete={false}
+                    title="Compiled Diagnoses"
+                    subtitle="Please order the diagnoses by priority"
+                    filter={filterCompiled}
+                    itemWrapper={((card, { item: d, index: i }) => {
+                        // const onChange = (value: any) => setDiagnoses(diagnoses.map((d, index) => index !== i ? d : {
+                        //     ...d,
+                        //     ...value,
+                        // }));
 
-                    // const disablePrimary = !!diagnoses.filter(d => d.isPrimaryProvisionalDiagnosis).length;
-                    // const disableSecondary = !!diagnoses.filter(d => d.isSecondaryProvisionalDiagnosis).length;
+                        // const disablePrimary = !!diagnoses.filter(d => d.isPrimaryProvisionalDiagnosis).length;
+                        // const disableSecondary = !!diagnoses.filter(d => d.isSecondaryProvisionalDiagnosis).length;
 
-                    // const onPrimary = () => onChange({ 
-                    //     isPrimaryProvisionalDiagnosis: true,
-                    //     isSecondaryProvisionalDiagnosis: false, 
-                    // });
-                    // const onSecondary = () => onChange({ 
-                    //     isPrimaryProvisionalDiagnosis: false,
-                    //     isSecondaryProvisionalDiagnosis: true, 
-                    // });
-                    // const onOther = () => onChange({ 
-                    //     isPrimaryProvisionalDiagnosis: false,
-                    //     isSecondaryProvisionalDiagnosis: false, 
-                    // });
+                        // const onPrimary = () => onChange({ 
+                        //     isPrimaryProvisionalDiagnosis: true,
+                        //     isSecondaryProvisionalDiagnosis: false, 
+                        // });
+                        // const onSecondary = () => onChange({ 
+                        //     isPrimaryProvisionalDiagnosis: false,
+                        //     isSecondaryProvisionalDiagnosis: true, 
+                        // });
+                        // const onOther = () => onChange({ 
+                        //     isPrimaryProvisionalDiagnosis: false,
+                        //     isSecondaryProvisionalDiagnosis: false, 
+                        // });
 
-                    return (
-                        <Box>
-                            <Box
-                                flexDirection="row"
-                                alignItems="center"
-                            >
-                                <Box flex={1}>{card}</Box>
-                                <Box style={{ width: 110 }}>
-                                    <Dropdown
-                                        title={d?.name}
-                                        placeholder="Move"
-                                        options={diagnoses.filter(filterCompiled).map((_, j) => {
-                                            const to = j + 1;
-                                            if ((to > diagnoses.filter(filterCompiled).length) || (i === j)) return null;
-                                            return {
-                                                value: `${j}`,
-                                                label: `Move to position ${to}`,
-                                            };
-                                        }).filter(o => o) as DropdownOption[]}
-                                        value={pickerValue}
-                                        onChange={value => {
-                                            if (!isNaN(Number(value))) {
-                                                const d = [...diagnoses].filter((_, j) => j === i)[0];
-                                                const items = [...diagnoses].filter((_, j) => j !== i);
-                                                items.splice(Number(value), 0, d);
-                                                setDiagnoses(items.map((d, i) => ({ ...d, priority: i + 1, })));
-                                            }
-                                            setPickerValue('');
-                                        }} 
-                                    />
+                        return (
+                            <Box>
+                                <Box
+                                    flexDirection="row"
+                                    alignItems="center"
+                                >
+                                    <Box flex={1}>{card}</Box>
+                                    <Box style={{ width: 110 }}>
+                                        <Dropdown
+                                            title={d?.name}
+                                            placeholder="Move"
+                                            options={diagnoses.filter(filterCompiled).map((_, j) => {
+                                                const to = j + 1;
+                                                if ((to > diagnoses.filter(filterCompiled).length) || (i === j)) return null;
+                                                return {
+                                                    value: `${j}`,
+                                                    label: `Move to position ${to}`,
+                                                };
+                                            }).filter(o => o) as DropdownOption[]}
+                                            value={pickerValue}
+                                            onChange={value => {
+                                                if (!isNaN(Number(value))) {
+                                                    const d = [...diagnoses].filter((_, j) => j === i)[0];
+                                                    const items = [...diagnoses].filter((_, j) => j !== i);
+                                                    items.splice(Number(value), 0, d);
+                                                    setDiagnoses(items.map((d, i) => ({ ...d, priority: i + 1, })));
+                                                }
+                                                setPickerValue('');
+                                            }} 
+                                        />
+                                    </Box>
                                 </Box>
                             </Box>
-                        </Box>
-                    );
-                })}
-            />
+                        );
+                    })}
+                />
 
-            <DiagnosesList
-                {...props}
-                divider
-                title="Diagnoses rejected"
-                filter={d => d.how_agree === 'No'}
-            />
-        </Box>
+                <DiagnosesList
+                    {...props}
+                    divider
+                    title="Diagnoses rejected"
+                    filter={d => d.how_agree === 'No'}
+                />
+            </Box>
+        </ScrollView>
     );
 }
