88eca567e7300efbb1b7048e70f43cb6f1823f70
farai <lafarai.dev@gmail.com>
2024-07-03 17:14:50 +0200

    header title

--- src/Home/Script/navOptions.tsx
+++ src/Home/Script/navOptions.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TouchableOpacity, Platform } from 'react-native';
+import { TouchableOpacity, Platform, View } from 'react-native';
 import { DrawerNavigationOptions } from '@react-navigation/drawer';
 import { NativeStackNavigationOptions } from '@react-navigation/native-stack';
 import Icon from '@expo/vector-icons/MaterialIcons';
@@ -35,26 +35,6 @@ const headerLeftPlaceholder: (params: GetNavOptionsParams) => DrawerNavigationOp
 		);
 	};
 
-const headerTitle: (params: GetNavOptionsParams) => DrawerNavigationOptions['headerTitle'] = ({ script, activeScreen, moreNavOptions }) => 
-	({ tintColor }) => {
-		return (
-			<Box>
-				<Text
-					style={{ color: tintColor }}
-					variant="title3"
-					numberOfLines={1}
-				>{moreNavOptions?.title || (activeScreen ? activeScreen?.data?.title : script?.data?.title)}</Text>
-				{!activeScreen ? null : (
-					<Text
-						color="textSecondary"
-						variant="caption"
-						numberOfLines={1}
-					>{script?.data?.title}</Text>
-				)}
-			</Box>
-		);
-	};
-
 const headerLeft: (params: GetNavOptionsParams) => DrawerNavigationOptions['headerLeft'] = ({ goBack }) => 
 	({ tintColor }) => {
 		return (
@@ -76,9 +56,9 @@ function RightActions({ color, screen, confirmExit }: { color?: string; screen: 
 
 	return (
 		<>
-			<Box marginRight="m" flexDirection="row">
+			<Box flexDirection="row" justifyContent="flex-end" columnGap="s">
 				{!!screen?.data?.infoText && (
-					<>
+					<Box marginRight="s">
 						<TouchableOpacity onPress={() => setOpenInfoModal(true)}>
 							<Icon 
 								name="info" 
@@ -86,9 +66,7 @@ function RightActions({ color, screen, confirmExit }: { color?: string; screen: 
 								color={color}
 							/>
 						</TouchableOpacity>
-
-						<Box marginLeft="s" />
-					</>
+					</Box>
 				)}
 
 				<TouchableOpacity onPress={() => setOpenModal(true)}>
@@ -144,19 +122,65 @@ const headerRight: (params: GetNavOptionsParams) => DrawerNavigationOptions['hea
 		return <RightActions color={tintColor} screen={activeScreen} confirmExit={confirmExit} />;
 	};
 
+const headerTitle: (params: GetNavOptionsParams) => DrawerNavigationOptions['headerTitle'] = 
+    ({ script, activeScreen, moreNavOptions, goBack, confirmExit }) => 
+        ({ tintColor }) => {
+            const title = moreNavOptions?.title || (activeScreen ? activeScreen?.data?.title : script?.data?.title);
+            return (
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                    }}
+                >
+                    {!!script && (
+                        <Box marginRight="s">
+                            <TouchableOpacity onPress={() => goBack()}>
+                                <Icon 
+                                    name={Platform.OS === 'ios' ? 'arrow-back-ios' : 'arrow-back'}  
+                                    size={28} 
+                                    color={tintColor}
+                                />
+                            </TouchableOpacity>
+                        </Box>
+                    )}
+
+                    <Box flex={1}>
+                        <Text
+                            style={{ color: tintColor }}
+                            variant="title3"
+                            numberOfLines={1}
+                        >{title}</Text>
+                        {!activeScreen ? null : (
+                            <Text
+                                color="textSecondary"
+                                variant="caption"
+                                numberOfLines={1}
+                            >{script?.data?.title}</Text>
+                        )}
+                    </Box>
+                    
+                    {!!script && (
+                        <Box>
+                            <RightActions color={tintColor} screen={activeScreen} confirmExit={confirmExit} />
+                        </Box>
+                    )}
+                </View>
+            );
+        }; 
+
 export function getNavOptions(params: GetNavOptionsParams) {
 	const opts: Partial<NativeStackNavigationOptions> = {};
 
 	if (!params.script) {
 		opts.headerTitle = headerTitlePlaceholder(params);
-		opts.headerLeft = headerLeftPlaceholder(params);
-		opts.headerRight = () => null;
+		// opts.headerLeft = headerLeftPlaceholder(params);
 	} else {
 		opts.headerTitle = headerTitle(params);
-		opts.headerLeft = headerLeft(params);
-		opts.headerRight = params.moreNavOptions?.hideHeaderRight ? () => null : (
-			params.moreNavOptions?.headerRight || headerRight(params)
-		);
+		// opts.headerLeft = headerLeft(params);
+        // opts.headerRight = params.moreNavOptions?.hideHeaderRight ? () => null : (
+		// 	params.moreNavOptions?.headerRight || headerRight(params)
+		// );
 	}
 	
 	return opts;
