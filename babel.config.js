134afd83bc4627f010e7afc0ce61aa12be417d20
farai <lafarai.dev@gmail.com>
2024-08-05 13:48:26 +0200

    upgrade to SDK 51

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,9 @@
-module.exports = function(api) {
-	api.cache(true);
-	return {
-		presets: ['babel-preset-expo'],
+module.exports = function (api) {
+    api.cache(true);
+    return {
+        presets: ['babel-preset-expo'],
 		plugins: [
-			[
-				"module-resolver",
-				{
-					"root": ["./"],
-					"alias": {
-						"@": "./"
-					}
-				},
-			],
 			'react-native-reanimated/plugin', // Reanimated plugin has to be the last item in the plugins array.
 		],
-	};
+    };
 };
