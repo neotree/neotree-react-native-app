c90d433628e9c5d4ab441e6ab30d9c1a4eac214f
farai <farai>
2024-10-15 16:43:55 +0200

    auth form

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,16 +1,14 @@
-import { View } from "react-native";
 import { router } from "expo-router";
 
 import { Text } from "@/components/ui/text";
 import { Button } from "@/components/ui/button";
-import { useHospitals } from "@/hooks/use-hospitals";
 import { Poster } from "@/components/poster";
 import { Content } from "@/components/content";
 
 export default function OnboardingScreen() {
     return (
         <Poster>
-            <Content>
+            <Content className="py-10">
                 <Text>Hello!</Text>
                 
                 <Button
