486d731c6c3e0d57f8e4b81c2a5e9f9e8090175a
farai <farai>
2025-03-12 11:21:16 +0200

    updates

--- src/types.ts
+++ src/types.ts
@@ -150,6 +150,8 @@ export type ScreenEntryValue = {
   prePopulate?: any[];
   printable?: boolean;
   extraLabels?: string[];
+  selected?: boolean;
+  data?: any;
 };
 
 export type ScreenEntry = {
