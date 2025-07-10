c07df25accb114c9f1bdcc81e607ecbe2e78f392
mbaradza <mbaradza1@gmail.com>
2025-07-10 14:10:25 +0200

    Fix conditional expression last screen evaluation

--- src/contexts/script/index.tsx
+++ src/contexts/script/index.tsx
@@ -470,7 +470,6 @@ function useScriptContextValue(props: ScriptContextProviderProps) {
 
             const condition: string = lastScreen?.data?.condition || '';
             const conditionSplit = condition.split('\n').map(c => c.trim()).filter(c => c);
-    
             if (condition) {
                 const parsedCondition = parseCondition(condition, entries.filter(e => e.screen.id !== lastScreen.id));
                 let conditionMet = evaluateCondition(parsedCondition);
@@ -483,14 +482,13 @@ function useScriptContextValue(props: ScriptContextProviderProps) {
                         if (!isTrue) conditionMet = false;
                     });
                 }
-
                 if (!conditionMet) {
                     lastScreenIndex = lastScreenIndex + 1;
                     lastScreen = lastScreenIndex >= screens.length ? null : getLastScreen(lastScreenIndex);
                 }
             }
-
-            return lastScreen;
+            
+            return screens[lastScreenIndex];
         };
     
         return getLastScreen(activeScreenIndex) || activeScreen;
@@ -800,8 +798,9 @@ function useScriptContextValue(props: ScriptContextProviderProps) {
         });
 
         const lastScreen = { ...getLastScreen() };
+        
         const lastScreenIndex = screens.map(s => `${s.id}`).indexOf(`${lastScreen?.id}`);
-
+        console.log("....ERE...",lastScreenIndex)
         const next = getScreen({ direction: 'next' });
         const nextScreen = next?.screen || lastScreen;
         const nextScreenIndex = next?.screen ? next?.index : lastScreenIndex;
@@ -811,8 +810,9 @@ function useScriptContextValue(props: ScriptContextProviderProps) {
             setLoadingScreen(false);
             return;
         }
-
-        if (activeScreen?.id === lastScreen?.id) {
+       
+        if ((activeScreen?.id === lastScreen?.id) && 
+         cachedEntries?.filter(e => `${e.screenIndex}` === `${lastScreenIndex}`).length>0) {
             if (reviewConfigurations?.length > 0) {
                 setReview(true)
                 setLastPage(lastScreen)
