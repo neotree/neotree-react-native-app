83fefef30f971ff69a3b29b00744af8278919f0a
mbaradza <mbaradza1@gmail.com>
2025-07-24 12:28:40 +0200

    Fix Undefined On Dates

--- src/utils/formatDate.tsx
+++ src/utils/formatDate.tsx
@@ -26,10 +26,13 @@ export function formatDate(date: any): string {
   }
   
   export function getDaysDifference(date1: Date, date2: Date): number {
-    const startOfDay1 = new Date(date1.getFullYear(), date1.getMonth(), date1.getDate());
-    const startOfDay2 = new Date(date2.getFullYear(), date2.getMonth(), date2.getDate());
+    const startOfDay1 = new Date(date1?.getFullYear(), date1?.getMonth(), date1?.getDate());
+    const startOfDay2 = new Date(date2?.getFullYear(), date2?.getMonth(), date2?.getDate());
+    if(startOfDay1 && startOfDay2){
     const diffInMs = startOfDay1.getTime()-startOfDay2.getTime();
     const msInOneDay = 1000 * 60 * 60 * 24;
     return Math.round(diffInMs / msInOneDay);
+    }
+    return 0
   }
   
