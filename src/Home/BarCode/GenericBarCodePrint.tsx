15402876637472947e571466d3b796b8cadf160c
mbaradza <mbaradza1@gmail.com>
2026-01-29 17:35:11 +0200

    Add Optimised bar code printing

--- src/Home/BarCode/GenericBarCodePrint.tsx
+++ src/Home/BarCode/GenericBarCodePrint.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import {TouchableOpacity, Platform} from "react-native";
-import { View, Text, Button as Btn, StyleSheet } from 'react-native';
 import Icon from '@expo/vector-icons/MaterialIcons';
 import { Button } from "../../components/Button";
 import { Br } from "../../components/Br";
-import { Box } from "../../components/Theme";
+import { Box, Text } from "../../components/Theme";
 import { NeotreeIDInput } from "../../components/Form";
 import { PrintBarCode } from "../../components/Session/PrintBarCode";
 import { QRCodeScan } from '@/src/components/Session/QRScan/QRCodeScan';
@@ -26,6 +25,7 @@ export function PrintGenericBarCode({ navigation }: types.StackNavigationProps<t
      if(focused){
         setUID('')
         setSession(null)
+        setShowQR(false)
      }
      
     },[focused])
@@ -48,6 +48,10 @@ export function PrintGenericBarCode({ navigation }: types.StackNavigationProps<t
 
     const onQrRead = (qrtext: any) => {
        //TODO VALIDATE INCOMING SESSION TO TAKE ONLY THE UID FOR PRINTING
+        if (!qrtext) {
+            setShowQR(false);
+            return;
+        }
         if (qrtext) {
             setUID(qrtext);
             setSession({"uid":qrtext})
@@ -55,50 +59,38 @@ export function PrintGenericBarCode({ navigation }: types.StackNavigationProps<t
         setShowQR(false);
     };
 
-    // return (
-    //     <>
-    //         <NeotreeIDInput
-    //             label="NEOTREE ID"
-    //             onChange={uid => setUID(uid)}
-    //             value={uid}
-    //         />
-    //         <Br spacing='l' />
-    //         <Button 
-    //             color="primary"
-    //             onPress={() => openQRscanner()}>
-    //             Scan QR
-    //         </Button>
-    //         {showQR ? <QRCodeScan onRead={onQrRead} generic={true}/> : null}
-    //         <Br spacing='l' />
-    //         {uid &&  <PrintBarCode
-    //             session={session}
-    //             isGeneric={true}
+    const handlePrinted = () => {
+        setUID('');
+        setSession(null);
+    };
 
-    //         />}
-            
-    //     </>
-    // );
-        return (
-            <View style={styles.container}>
-              <Text style={styles.message}>🚧 This feature is going to be used to print QR CODES.It is coming soon! 🚧</Text>
-              <Btn color={'maroon'} title="GET ME OUT OF HERE!!" onPress={() => navigation.navigate('Home')} />
-            </View>
-          );
+    return (
+        <Box flex={1} padding="l">
+            <Text variant="title2" color="primary">Print QR Code</Text>
+            <Br spacing="m" />
+            <NeotreeIDInput
+                label="NEOTREE ID"
+                onChange={uid => {
+                    setUID(uid);
+                    setSession(uid ? { uid } : null);
+                }}
+                value={uid}
+            />
+            <Br spacing='l' />
+            <Button 
+                color="primary"
+                onPress={() => openQRscanner()}>
+                Scan QR
+            </Button>
+            {showQR ? <QRCodeScan onRead={onQrRead} generic={true}/> : null}
+            <Br spacing='l' />
+            {uid &&  <PrintBarCode
+                session={session}
+                isGeneric={true}
+                onPrinted={handlePrinted}
+            />}
+        </Box>
+    );
 
 
 }
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      padding: 20,
-      backgroundColor: '#ffeeba',
-      borderRadius: 8,
-      margin: 20,
-    },
-    message: {
-      fontSize: 24, 
-      marginBottom: 10,
-    }
-  });
